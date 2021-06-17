966f445717cab9de4f918449414366de8f55dbf5
KungumaGangai <gangaibtechit@gmail.com>
2021-06-17 17:11:48 +0530

    working on dropzone and upload image

--- 02/__init__.py
+++ 02/__init__.py
@@ -1,11 +1,32 @@
+import os 
+
 from flask import Flask
+from flask_dropzone import Dropzone
+# from flask_uploads import UploadSet
+# , configure_uploads, IMAGES, patch_request_class
 from . import extractor
 
 app = Flask(__name__)
 app.register_blueprint(extractor.bp)
 
+
+
+# Uploads settings
+# app.config['UPLOAD_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+
+# photos = UploadSet('photos', IMAGES)
+# configure_uploads(app, photos)
+# patch_request_class(app) #set maximum file size, default is 16MB
+
 @app.route('/')
 def extractor():
+    dropzone = Dropzone(app)
+
+    # Dropzone Settings
+    app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
+    app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+    app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+    app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
     return app
 
 @app.route('/hello')
