a411384dc6e952730b01744ce3928e1344dc530f
KungumaGangai <gangaibtechit@gmail.com>
2021-07-02 19:16:55 +0530

    tried img2pdf library

--- 02/extractor.py
+++ 02/extractor.py
@@ -1,5 +1,7 @@
 import os
+import img2pdf
 import pytesseract
+
 from PIL import Image
 from fpdf import FPDF
 
@@ -12,6 +14,9 @@ bp = Blueprint('extractor', __name__)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+file_name = None
+
+
 @bp.route('/')
 def index():
     title = "TxtExt"
@@ -19,6 +24,7 @@ def index():
 
 @bp.route('/extracttext')
 def extracttext():
+    global file_name 
     file_name = request.args['file_name']
        
     pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"    
@@ -31,20 +37,30 @@ def extracttext():
 @bp.route('/download', methods=['POST'])
 def download():
     if request.method == 'POST':
+        # using img2pdf library
+        img_path = r"E:/gitprojects/text-extractor/02/static/"+file_name
+        pdf_path = r"E:/gitprojects/text-extractor/02/static/data1.pdf"
+        image = Image.open(img_path)
+        pdf_bytes = img2pdf.convert(image.filename)
+        file = open(pdf_path, "wb")
+        file.write(pdf_bytes)
+        image.close()
+        file.close()
+
+        # export as .txt file
         content = request.form['edittext']
         doc = open('E:/gitprojects/text-extractor/02/static/extracted.txt', 'w')
         doc.write(content)
         doc.close()
 
         # export as pdf process
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font('Arial', size=15)
-        # pdf.cell(200, 10, txt=content, ln=1, align='C')
-        for x in content:
-            pdf.cell(200, 10, txt = x, ln = 1, align = 'C')
-   
-        pdf.output("IMG.pdf")
+        # pdf = FPDF()
+        # pdf.add_page()
+        # pdf.set_font('Arial', size=15)
+        # # pdf.cell(200, 10, txt=content, ln=1, align='C')
+        # for x in content:
+        #     pdf.cell(200, 10, txt = x, ln = 1, align = 'C')   
+        # pdf.output("IMG.pdf")
         
 
         return render_template('makeMeme.html', content=content)
